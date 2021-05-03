eb9d44f3480626ed0459be64da8c2a2c4f441497
Manav Verma <vmanav1999@gmail.com>
2021-05-03 22:11:42 +0530

    In railway-management-service, a some new updates in TrainController Functionality & Testing Completed

---
+++ user-management-service/src/test/java/user/management/system/MainTests.java
@@ -0,0 +1,13 @@
+package user.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
